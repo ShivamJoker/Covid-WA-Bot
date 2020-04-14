b360a256f90c02eece8e862065dc3916958cd636
Shivam <SINGHSHIVAMKR@GMAIL.COM>
2020-04-14 22:51:16 +0530

    bot made

--- functions/getData.js
+++ functions/getData.js
@@ -1,6 +1,19 @@
-exports.handler = function(event, context, callback){
+const axios = require("axios");
+exports.handler = function (event, context, callback) {
+  const url = `https://api.rootnet.in/covid19-in/stats/latest`;
+  axios.get(url).then((res) => {
+    const summary = res.data.data.summary;
+
+    const msg = {
+      replies: [
+        { message: `Confirmed cases ${summary.total} ✅` },
+        { message: `Deaths ${summary.deaths} 🏴‍☠️` },
+        { message: `Recovered ${summary.discharged} 🥳` },
+      ],
+    };
     callback(null, {
-        statusCode: 200,
-        body: 'Hello bae'
-    })
-}
+      statusCode: 200,
+      body: JSON.stringify(msg),
+    });
+  });
+};
